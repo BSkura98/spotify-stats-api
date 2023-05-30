3d6788f92ff0947b6583938a50109336c37453ca
Bartłomiej Skura <bartek33s@wp.pl>
2023-05-30 09:08:06 +0200

    Add most popular days to song stats

--- service/getSongStats/response.ts
+++ service/getSongStats/response.ts
@@ -1,7 +1,13 @@
+export interface DayWithFullPlays {
+  date: string;
+  fullSongPlays: number;
+}
+
 export interface GetSongStatsResponse {
   artistName: string;
   trackName: string;
   firstPlay: Date;
   totalMsPlayed: number;
   fullSongPlays: number;
+  mostPopularDays: DayWithFullPlays[];
 }
--- service/getSongStats/service.ts
+++ service/getSongStats/service.ts
@@ -1,3 +1,5 @@
+import { isSameDay, startOfDay } from "date-fns";
+
 import { NotFoundError } from "../../errors/NotFoundError";
 import { ITrackPlay, TrackPlay } from "../../models/TrackPlay";
 import { GetSongStatsBody } from "./request";
@@ -17,6 +19,41 @@ const getFirstMatchingResultInSpotify = (
         .map((artist: { name: string }) => artist.name)
         .includes(artistName)
   )[0];
+
+const getMostPopularDays = (trackPlays: ITrackPlay[], durationMs: number) =>
+  trackPlays
+    .reduce(
+      (
+        days: { date: Date; totalMsPlayed: number }[],
+        trackPlay: ITrackPlay
+      ) => {
+        let dayFound = false;
+        days = days.map((day) => {
+          if (isSameDay(day.date, trackPlay.endTime)) {
+            dayFound = true;
+            return {
+              ...day,
+              totalMsPlayed: day.totalMsPlayed + trackPlay.msPlayed,
+            };
+          }
+          return day;
+        });
+        if (!dayFound) {
+          days.push({
+            date: trackPlay.endTime,
+            totalMsPlayed: trackPlay.msPlayed,
+          });
+        }
+        return days;
+      },
+      []
+    )
+    .sort((day1, day2) => day2.totalMsPlayed - day1.totalMsPlayed)
+    .slice(0, 10)
+    .map((day) => ({
+      date: startOfDay(day.date).toLocaleDateString("pl-PL"),
+      fullSongPlays: day.totalMsPlayed / durationMs,
+    }));
 
 export const getSongStatsService = async (
   request: GetSongStatsBody
@@ -47,11 +84,17 @@ export const getSongStatsService = async (
   const fullSongPlays =
     totalMsPlayed / firstMatchingResultInSpotify.duration_ms;
 
+  const mostPopularDays = getMostPopularDays(
+    trackPlays,
+    firstMatchingResultInSpotify.duration_ms
+  );
+
   return {
     artistName,
     trackName,
     firstPlay,
     totalMsPlayed,
     fullSongPlays,
+    mostPopularDays,
   };
 };
