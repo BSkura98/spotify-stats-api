0973b6d27dbd30157342ccd322c482b5a87ade3d
Bartłomiej Skura <bartek33s@wp.pl>
2023-06-01 19:47:40 +0200

    Small code improvements

--- database/trackPlays.ts
+++ database/trackPlays.ts
@@ -2,11 +2,8 @@ import testDatabase from "../tests/database/testDatabase";
 
 import { TrackPlay } from "../models/TrackPlay";
 
-const getTrackPlays = async () => {
-  return TrackPlay.find({});
-};
+const getTrackPlays = async () => TrackPlay.find({});
 
-const database =
-  process.env.NODE_ENV === "test" ? testDatabase : { getTrackPlays };
-
-export default { ...database };
+export default process.env.NODE_ENV === "test"
+  ? testDatabase
+  : { getTrackPlays };
